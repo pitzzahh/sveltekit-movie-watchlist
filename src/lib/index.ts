522f3187f07a8b3806b153311097553403caae37
Peter John Arao <pitzzahh@gmail.com>
2023-11-16 08:13:59 +0000

    ✨ Add suggestGenre function and form info when adding movies

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,45 @@
-// place files you want to import through the `$lib` alias in this folder.
+export async function suggestGenre(value: string): Promise<string | null> {
+	const lowercasedValue = value.toLowerCase();
+
+	try {
+		const response = await fetch('/api/genres');
+		const genres = await response.json();
+		console.log(genres);
+		const similarGenres = genres.filter(
+			(genre: string) => genre.includes(lowercasedValue) || lowercasedValue.includes(genre)
+		);
+
+		if (similarGenres.length > 0) {
+			return similarGenres[0];
+		}
+
+		return null;
+	} catch (error) {
+		console.error('Error fetching genres:', error);
+		return null;
+	}
+}
+
+export const movieFormInfo = [
+	{
+		name: 'title',
+		label: 'Title',
+		description: 'The title of the movie'
+	},
+	{
+		name: 'genre',
+		label: 'Genre',
+		description: 'The genre of the movie'
+	},
+	{
+		name: 'year',
+		label: 'Year of release',
+		description: 'The year the movie was released'
+	},
+    {
+        name: 'rating',
+        label: 'Rating',
+        description: 'The rating of the movie'
+    },
+    
+];
