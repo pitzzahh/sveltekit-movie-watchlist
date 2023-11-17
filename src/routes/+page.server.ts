b7dcf397eb04d7b2b37d124af1c7d36e168487ed
Peter John Arao <pitzzahh@gmail.com>
2023-11-17 11:50:09 +0000

    🔥 Removed createMovie action, moved to a new route

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,5 @@
 import type { PageServerLoad, Actions } from './$types';
-import { movies } from '$db/movies';
-import { superValidate } from 'sveltekit-superforms/server';
-import { formSchema } from './movie/schema';
-import { fail } from '@sveltejs/kit';
+import { movies } from '$db/collections';
 
 export const load = (async () => {
 	const data = (await movies.find({}).toArray()).map((movie) => ({
@@ -12,34 +9,11 @@ export const load = (async () => {
 
 	return {
 		status: data.length ? 200 : 404,
-		movies: data,
-		form: superValidate(formSchema)
+		movies: data
 	};
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-	createMovie: async (event) => {
-		const form = await superValidate(event, formSchema);
-		if (!form.valid) {
-			return fail(400, {
-				form
-			});
-		}
-		const result = await movies.insertOne({
-			id: null,
-			title: form.data.title,
-			genre: form.data.genre,
-			year: Number(form.data.year),
-			rating: Number(form.data.rating),
-			watched: false
-		});
-
-		console.log('submitted form');
-		return {
-			form,
-			Tresult: JSON.stringify(result)
-		};
-	},
 	deleteMovie: async (event) => {
 		console.log('deleting movie');
 		const data = await event.request.formData();
