b8124befb65e441d2b249df5b46fb57215723b3b
Peter John Arao <pitzzahh@gmail.com>
2023-11-17 13:01:35 +0000

    ✨ Add a function that adds a movie

--- src/db/collections.ts
+++ src/db/collections.ts
@@ -2,3 +2,24 @@ import db from '$db/mongo';
 
 export const movies = db.collection<Movie>('movies');
 export const genres = db.collection<Genre>('genres');
+
+/**
+ * Adds a new movie to the 'movies' collection.
+ * @param movieData - The data for the new movie.
+ * @returns Promise<string> - Resolves with the ID of the added movie.
+ */
+export const addMovie = (movieData: Movie): Promise<string> => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const result = await movies.insertOne(movieData);
+
+			if (result.acknowledged) {
+				resolve(result.insertedId.toString());
+			} else {
+				reject(`Error adding movie: ${result}`);
+			}
+		} catch (error) {
+			reject(`Error adding movie: ${error}`);
+		}
+	});
+};
