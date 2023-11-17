029c494e8e7a70f308b8d5617fe93792dc761070
Peter John Arao <pitzzahh@gmail.com>
2023-11-17 12:24:47 +0000

    ✨ Changed Theme swticher button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.postcss';
 	import { Button } from '$lib/components/ui/button';
 	import { Sun, Moon } from 'lucide-svelte';
-	import { toggleMode } from 'mode-watcher';
+	import { setMode, resetMode } from 'mode-watcher';
 	import { onMount } from 'svelte';
 	import { store } from '$lib';
 	import { page } from '$app/stores';
@@ -37,13 +37,15 @@
 	});
 
 	let buttonText = 'Add Movie';
-	
+
 	$: {
 		buttonText = $page.route.id === '/addMovie' ? 'Home' : 'Add Movie';
 	}
 </script>
 
-<nav class="flex justify-between items-center p-4 sticky top-0">
+<nav
+	class="flex justify-between items-center p-4 sticky top-0 z-10 backdrop-filter backdrop-blur-lg bg-opacity-20"
+>
 	<div class="flex items-center">
 		<a href="/" class="font-bold text-2xl md:text-3xl cursor-pointer">Movie Watchlist</a>
 	</div>
@@ -55,16 +57,24 @@
 			</Button>
 		</a>
 
-
-		<Button on:click={toggleMode} variant="outline" size="icon">
-			<Sun
-			  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-			/>
-			<Moon
-			  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		  </Button>
+		<DropdownMenu.Root positioning={{ placement: 'bottom-end' }}>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button builders={[builder]} variant="outline" size="icon">
+					<Sun
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 	</div>
 </nav>
 
