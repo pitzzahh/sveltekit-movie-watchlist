4e40dabb72323992f1b7eaf992af78bfac192d54
Peter John Arao <pitzzahh@gmail.com>
2023-11-17 12:27:36 +0000

    🚚 Add page for showing form

---
+++ src/routes/addMovie/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+	import MovieForm from './MovieForm.svelte';
+    
+    export let data: PageData;
+</script>
+
+<MovieForm form={data.form} />
