25d78d2500ac01b3408e5763991f590299837854
Peter John Arao <pitzzahh@gmail.com>
2023-11-18 07:26:01 +0000

    ✨ Add func to addGenres to db

--- src/db/collections.ts
+++ src/db/collections.ts
@@ -1,5 +1,5 @@
 import db from '$db/mongo';
-import type { InsertOneResult } from 'mongodb';
+import type { InsertManyResult, InsertOneResult } from 'mongodb';
 
 export const movies = db.collection<Movie>('movies');
 export const genres = db.collection<Genre>('genres');
@@ -24,6 +24,46 @@ export const addMovie = (movieData: Movie): Promise<string> => {
 		}
 	});
 };
+
+
+/**
+ * Adds a new movie to the 'movies' collection.
+ * @param movieData - The data for the new movie.
+ * @returns Promise<string> - Resolves with the ID of the added movie.
+ */
+export const addGenres = (genre: Genre[]): Promise<string[]> => {
+	return new Promise(async (resolve, reject) => {
+		try {
+
+			// check if genre already exists in db
+			// if it does, add the id to the genreIds array
+			let genreIds: string[] = [];
+			genre.forEach((genre) => {
+				genres.findOne({ name: genre.name }).then((result) => {
+					if (result) {
+						genreIds.push(result._id.toString());
+					}
+					else {
+						genres.insertOne(genre).then((result) => {
+							genreIds.push(result.insertedId.toString());
+						});
+					}
+				});
+			});
+
+			const result: InsertManyResult<Genre> = await genres.insertMany(genre);
+			
+			if (result.insertedCount === genre.length && result.acknowledged) {
+				resolve(genreIds);
+			} else {
+				reject(`Error adding movie: ${result}`);
+			}
+		} catch (error) {
+			reject(`Error adding movie: ${error}`);
+		}
+	});
+};
+
 
 export const fetchDataFromMongoDB = async (collection: any) => {
     return await collection.find({}).toArray();
